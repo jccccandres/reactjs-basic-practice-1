4b17fc14cf5f1cfa383290383a3bed89adcae08c
jcandres <jc.andres17@gmail.com>
2022-08-08 18:04:57 +0800

    Update nav and components

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -3,9 +3,7 @@ import React from "react";
 const Todolist = () => {
     return (
         <>
-            <div className="main">
-                <h1>Todolist Page</h1>
-            </div>
+            <h1>Todolist Page</h1>
         </>
     ); 
 }
